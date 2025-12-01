3c56d67f3571318bdfff0ab1f81fa1fa7ff82f37
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-01 17:51:29 +0300

    Добавил реализации методов  1. Для обновления поста (сразу с тегами) update(Post post)  2. Для сохранения поста (с тегами) save(Post post)

--- src/main/java/com/alex/blog/repository/impl/JdbcNativePostRepositoryImpl.java
+++ src/main/java/com/alex/blog/repository/impl/JdbcNativePostRepositoryImpl.java
@@ -18,6 +18,7 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
@@ -48,26 +49,22 @@ public class JdbcNativePostRepositoryImpl implements PostRepository {
 
     }
 
-    @Override
-    public Post save(Post post) {
-        return null;
-    }
 
     @Override
     public boolean delete(Long id) {
-        return false;
+        String sqlDelPosts = """
                 DELETE FROM posts WHERE id = ?
+                """;
+
+        return jdbcTemplate.update(sqlDelPosts, id) > 0;
     }
 
-    @Override
-    public boolean update(Post post) {
-        return false;
-    }
 
     @Override
     public Page<Post> findAll(Criteria criteria) {
         return null;
     }
+
 
     @Override
     public Long incrementLikesCount(Long postId) {
@@ -120,6 +117,7 @@ public class JdbcNativePostRepositoryImpl implements PostRepository {
 
         };
     }
+
     private List<String> getTags(ResultSet rs) throws SQLException {
         return Optional.ofNullable(rs.getString(Post.Fields.tags))
                 .map(ts -> ts.split(","))
@@ -127,7 +125,105 @@ public class JdbcNativePostRepositoryImpl implements PostRepository {
                 .orElseGet(Collections::emptyList);
 
     }
+    @Override
+    public boolean update(Post post) {
+        String sqlUpdate = """
+                UPDATE posts SET title=?, text=?
+                WHERE id=?
+                """;
 
+        Post updatedPost = execModifySqlWithFullReturn(post, sqlUpdate);
+
+
+        String sqlDelTags = """
+                DELETE FROM tags AS t
+                USING posts_tags AS pt
+                WHERE pt.tag_id=t.id
+                AND pt.post_id=?
+                """;
+
+        jdbcTemplate.update(sqlDelTags, post.getId());
+
+        insertTagsAndLink(updatedPost.getId(),post.getTags());
+
+        updatedPost.setTags(post.getTags());
+
+        return true;
+    }
+
+
+    private Post execModifySqlWithFullReturn(Post post, String sqlModify) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(sqlModify, new String[]{"id", "title", "text", "comments_count", "likes_count"});
+            ps.setString(1, post.getTitle());
+            ps.setString(2, post.getText());
+            if (post.getId() != null) {
+                ps.setLong(3, post.getId());
+            }
+            return ps;
+        }, keyHolder);
+
+        return map(keyHolder);
+    }
+
+    private void insertTagsAndLink(Long postId, List<String> tags) {
+
+        SqlParameterSource[] batchArgs = tags.stream()
+                .map(tagName -> new MapSqlParameterSource("tagName", tagName))
+                .toArray(SqlParameterSource[]::new);
+
+        String sqlInsertTags = """
+                INSERT INTO tags (name)
+                SELECT :tagName
+                WHERE NOT EXISTS (SELECT 1 FROM tags WHERE name = :tagName)
+                """;
+
+        namedParameterJdbcTemplate.batchUpdate(sqlInsertTags, batchArgs);
+
+
+        SqlParameterSource[] batchArgs1 = tags.stream()
+                .map(tagName -> new MapSqlParameterSource()
+                        .addValue("postId", postId)
+                        .addValue("tagName", tagName))
+                .toArray(SqlParameterSource[]::new);
+
+        String sqlLinkTagsToPost = """
+                INSERT INTO posts_tags (post_id, tag_id)
+                SELECT  :postId, id FROM tags
+                WHERE name = :tagName
+                """;
+
+
+        namedParameterJdbcTemplate.batchUpdate(sqlLinkTagsToPost, batchArgs1);
+    }
+
+    @Override
+    public Post save(Post post) {
+        String sqlInsertPost = """
+                INSERT INTO posts (title, text)
+                 VALUES(?,?)
+                """;
+        Post savedPost = execModifySqlWithFullReturn(post, sqlInsertPost);
+
+        insertTagsAndLink(savedPost.getId(), post.getTags());
+
+        savedPost.setTags(post.getTags());
+        return savedPost;
+
+    }
+
+    private Post map(KeyHolder keyHolder) {
+        return keyHolder.getKeyList().stream().map(m -> {
+            Post savedPost = new Post();
+            savedPost.setId((Long) m.get(Post.Fields.id));
+            savedPost.setTitle((String) m.get(Post.Fields.title));
+            savedPost.setText((String) m.get(Post.Fields.text));
+            savedPost.setLikesCount((Long) m.get("likes_count"));
+            savedPost.setCommentsCount((Long) m.get("comments_count"));
+            return savedPost;
+        }).findFirst().orElseThrow();
+    }
 
 
 
