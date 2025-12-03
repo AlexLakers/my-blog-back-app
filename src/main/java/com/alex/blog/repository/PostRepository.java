b26ef277eeff529c107b489b0b4c8853787f2ac7
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-04 00:07:57 +0300

    Добавил методы для проверки по ID (xistsByIdAndPostId(Long id, Long postId)). Сделал небольшой рефакторинг.

--- src/main/java/com/alex/blog/repository/PostRepository.java
+++ src/main/java/com/alex/blog/repository/PostRepository.java
@@ -1,15 +1,20 @@
 package com.alex.blog.repository;
 
 import com.alex.blog.model.Post;
+import com.alex.blog.search.Criteria;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.Optional;
 
 public interface PostRepository {
     Optional<Post> findById(Long id);
-
     Post save(Post post);
     void delete(Long id);
     Post update(Post post);
     Long incrementLikesCount(Long postId);
     void updateImagePath(Long postId, String imagePath);
     Long incrementCommentsCount(Long postId);
+    Page<Post> findPostsByCriteriaAndPageable(Criteria criteria, Pageable pageable);
+    boolean existsById(Long id);
 }
---
+++ src/main/java/com/alex/blog/repository/impl/JdbcNativeCommentRepositoryImpl.java
@@ -0,0 +1,114 @@
+package com.alex.blog.repository.impl;
+
+import com.alex.blog.model.Comment;
+import com.alex.blog.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class JdbcNativeCommentRepositoryImpl implements CommentRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public Comment save(Comment comment) {
+        String sqlUpdate = """
+                            INSERT INTO comments (text, post_id)
+                             VALUES(?,?)
+                """;
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(sqlUpdate, new String[]{"id", "text", "post_id"});
+            ps.setString(1, comment.getText());
+            ps.setLong(2, comment.getPostId());
+            return ps;
+        }, keyHolder);
+        return map(keyHolder);
+    }
+
+    @Override
+    public void delete(Long id) {
+        String sqlDelete = """
+                    DELETE FROM comments WHERE id = ?
+                """;
+        jdbcTemplate.update(sqlDelete, id);
+    }
+
+    @Override
+    public Comment update(Comment comment) {
+        String sqlUpdate = """
+                UPDATE comments SET text=?, post_id=?
+                WHERE id=?
+                """;
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(sqlUpdate, new String[]{"id", "text", "post_id"});
+            ps.setString(1, comment.getText());
+            ps.setLong(2, comment.getPostId());
+            ps.setLong(3, comment.getId());
+            return ps;
+        }, keyHolder);
+
+        return map(keyHolder);
+    }
+
+
+    @Override
+    public Optional<Comment> findById(Long id) {
+        String sql = """
+                 SELECT * FROM comments WHERE id = ?
+                """;
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject(sql, getRowMapper(), id));
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+
+    @Override
+    public Boolean existsByIdAndPostId(Long id, Long postId) {
+        String sqlExists=""" 
+                SELECT EXISTS (SELECT 1 FROM comments WHERE id = ? AND post_id = ?)
+                """;
+        return Boolean.TRUE.equals(jdbcTemplate.queryForObject(sqlExists, Boolean.class, id,postId));
+    }
+
+    @Override
+    public void deleteByPostId(Long postId) {
+        String sqlDelete= """
+                DELETE FROM comments WHERE post_id = ?
+                """;
+        jdbcTemplate.update(sqlDelete, postId);
+    }
+
+
+    private RowMapper<Comment> getRowMapper() {
+        return (rs, rc) -> {
+            Comment comment = new Comment();
+            comment.setId(rs.getLong(Comment.Fields.id));
+            comment.setText(rs.getString(Comment.Fields.text));
+            comment.setPostId(rs.getLong("post_id"));
+            return comment;
+
+        };
+    }
+    private Comment map(KeyHolder keyHolder) {
+        return keyHolder.getKeyList().stream().map(m -> {
+            Comment comment = new Comment();
+            comment.setId((Long) m.get(Comment.Fields.id));
+            comment.setText((String) m.get(Comment.Fields.text));
+            comment.setPostId((Long) m.get("post_id"));
+            return comment;
+        }).findFirst().orElseThrow();
+    }
+}
