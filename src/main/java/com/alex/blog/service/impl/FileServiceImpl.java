77350b539308a69a6195d1e0cf1703470bd7130e
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-04 20:52:03 +0300

    Добавил интерфейс FileService для работы с файловой системой. 1. Реализовал метод для сохранения файла saveFile(InputStream content, String folder, String fileName)

---
+++ src/main/java/com/alex/blog/service/impl/FileServiceImpl.java
@@ -0,0 +1,41 @@
+package com.alex.blog.service.impl;
+
+import com.alex.blog.service.FileService;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Optional;
+
+public class FileServiceImpl implements FileService {
+    @Value("${blog.image.base.dir:/blog}")
+    private String baseDir;
+
+
+    @SneakyThrows
+    @Override
+    public void saveFile(InputStream content, String folder, String fileName) {
+        Path fullPath = buildPath(folder, fileName);
+        Files.createDirectories(fullPath.getParent());
+        Files.createDirectories(fullPath.getParent());
+        Files.copy(content, fullPath, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    @Override
+    public boolean deleteFile(String folder, String fileName) {
+        return false;
+    }
+
+    @Override
+    public Optional<byte[]> getFile(String folder, String fileName) {
+        return Optional.empty();
+    }
+
+    private Path buildPath(String folder, String fileName) {
+        return Path.of(baseDir, folder, fileName);
+    }
+
+}
