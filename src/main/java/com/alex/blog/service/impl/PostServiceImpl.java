3711c4444c20a96a1260b0543901aeb44b2c7825
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-05 00:07:20 +0300

    Добавил интрйес PostService , в том числек и методы для работы с файлами.

---
+++ src/main/java/com/alex/blog/service/impl/PostServiceImpl.java
@@ -0,0 +1,137 @@
+package com.alex.blog.service.impl;
+
+import com.alex.blog.api.dto.PostCreateDto;
+import com.alex.blog.api.dto.PostReadDto;
+import com.alex.blog.api.dto.PostUpdateDto;
+import com.alex.blog.exception.EntityCreationException;
+import com.alex.blog.exception.EntityNotFoundException;
+import com.alex.blog.mapper.PostMapper;
+import com.alex.blog.model.Post;
+import com.alex.blog.repository.CommentRepository;
+import com.alex.blog.repository.PostManagementRepository;
+import com.alex.blog.repository.PostSearchRepository;
+import com.alex.blog.search.Criteria;
+import com.alex.blog.search.PostPageDto;
+import com.alex.blog.search.SearchDto;
+import com.alex.blog.service.FileService;
+import com.alex.blog.service.PostService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+
+    private final PostManagementRepository postManagementRepository;
+    private final PostSearchRepository postSearchRepository;
+    private final PostMapper postMapper;
+    private final CommentRepository commentRepository;
+    private final FileService fileService;
+
+
+    @Override
+    public PostReadDto findOnePost(Long postId) {
+        return postSearchRepository.findPostById(postId)
+                .map(postMapper::toPostReadDto)
+                .orElseThrow(() -> new EntityNotFoundException("The post not found by id:%d".formatted(postId)));
+
+    }
+
+    @Override
+    public Long incrementLikesCount(Long postId) {
+        if (!postManagementRepository.existsById(postId)) {
+            throw new EntityNotFoundException("The post not found by id:%d"
+                    .formatted(postId));
+        }
+        return postManagementRepository.incrementLikesCount(postId);
+    }
+
+    @Override
+    public PostPageDto findPageByCriteria(SearchDto searchDto) {
+        System.out.println(searchDto);
+        Map<Boolean, List<String>> tokens = Arrays.stream(searchDto.searchLine().split(" "))
+                .filter(Predicate.not(String::isEmpty))
+                .collect(Collectors.partitioningBy(token -> token.startsWith("#")));
+
+
+        List<String> tags = tokens.get(Boolean.TRUE).stream()
+                .map(t -> t.replace("#", ""))
+                .collect(Collectors.toList());
+
+        String title = tokens.get(Boolean.FALSE).stream()
+                .collect(Collectors.joining(" "));
+
+        Pageable pageable = PageRequest.of(searchDto.pageNumber(), searchDto.pageSize());
+        Page<Post> page = postSearchRepository.findPostsByCriteriaAndPageable(new Criteria(title, tags), pageable);
+
+
+        return buildPostPageDto(page);
+    }
+
+    @Override
+    public PostReadDto updatePost(Long postId, PostUpdateDto postUpdateDto) {
+
+        return postSearchRepository.findPostById(postId)
+                .map(post -> {
+                            postMapper.updatePost(postUpdateDto, post);
+                            return postManagementRepository.update(post);
+                        }
+                )
+                .map(postMapper::toPostReadDto)
+                .orElseThrow(() -> new EntityNotFoundException("The post with id:%1$d not found"
+                        .formatted(postId)));
+    }
+
+    public void deletePost(Long postId) {
+        if (!postManagementRepository.existsById(postId)) {
+            throw new EntityNotFoundException("The post with id:%1$d not found"
+                    .formatted(postId));
+        }
+        postManagementRepository.delete(postId);
+        commentRepository.deleteByPostId(postId);
+
+    }
+
+    @Override
+    public byte[] getImage(Long postId) {
+        String imagePath=postManagementRepository.getImagePath(postId);
+        System.out.println(imagePath);
+        return fileService.getFile(imagePath).get();
+    }
+
+    @SneakyThrows
+    @Override
+    public void updateImage(long postId, MultipartFile file) {
+        InputStream is=file.getInputStream();
+        String imageName= postId+"/"+(Objects.requireNonNull(file.getOriginalFilename()));
+
+        postManagementRepository.updateImagePath(postId, imageName);
+        fileService.saveFile(is,imageName);
+    }
+
+    public PostReadDto savePost(PostCreateDto postCreateDto) {
+        return Optional.ofNullable(postCreateDto)
+                .map(postMapper::toPost)
+                .map(postManagementRepository::save)
+                .map(postMapper::toPostReadDto)
+                .orElseThrow(() -> new EntityCreationException("An error occurred during saving a new post"));
+
+    }
+
+    private PostPageDto buildPostPageDto(Page<Post> page) {
+        List<PostReadDto> content = page.getContent().stream().map(postMapper::toPostReadDto).toList();
+        return new PostPageDto(content, page.hasPrevious(), page.hasNext(), page.getTotalPages() - 1);
+    }
+
+
+}
