311c4f10cf34628b29b2963552d0954ea1b66c14
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-05 06:35:38 +0300

    Добавил endpoint для инкремента лайков поста /{postId}/likes"

--- src/main/java/com/alex/blog/api/rest/controller/PostRestController.java
+++ src/main/java/com/alex/blog/api/rest/controller/PostRestController.java
@@ -8,6 +8,7 @@ import com.alex.blog.search.SearchDto;
 import com.alex.blog.service.PostService;
 import jdk.jfr.ContentType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -24,13 +25,29 @@ public class PostRestController {
     public final PostService postService;
 
 
+    @PutMapping(value = "/{postId}/image",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Void> updateImage(@PathVariable("postId") Long postId,
+                                            @RequestParam("image") MultipartFile image) {
+        postService.updateImage(postId, image);
+        return ResponseEntity.ok().build();
 
-    @GetMapping(value = "/{id}/image",produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<byte[]> getImage(@PathVariable Long id) {
-        byte[] image = postService.getImage(id);
-        return ResponseEntity.ok()
-                .contentType(MediaType.IMAGE_JPEG)
+    }
+
+    @GetMapping(value = "/{postId}/image")
+    public ResponseEntity<byte[]> getImage(@PathVariable("postId") Long postId) {
+        System.out.println("dsadsadasas");
+        byte[] image = postService.getImage(postId);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType( MediaType.APPLICATION_OCTET_STREAM)
                 .body(image);
+    }
+
+    @PatchMapping(path = "/{postId}/likes")
+    public ResponseEntity<Long> incrementLikesCount(@PathVariable("postId") Long postId) {
+       return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(postService.incrementLikesCount(postId));
     }
 }
 
