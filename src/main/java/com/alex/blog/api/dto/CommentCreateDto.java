729711e85c52721e169a013eabbaca3e31e832a1
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-05 07:25:22 +0300

    Добавил валидацию в дто для создания комментария

--- src/main/java/com/alex/blog/api/dto/CommentCreateDto.java
+++ src/main/java/com/alex/blog/api/dto/CommentCreateDto.java
@@ -1,4 +1,12 @@
 package com.alex.blog.api.dto;
 
-public record CommentCreateDto(String text, Long postId) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CommentCreateDto(
+                            @NotBlank(message = "The text should be not blank ")
+                            String text,
+
+                            @NotNull(message = "The postId should be not null")
+                            Long postId) {
 }
