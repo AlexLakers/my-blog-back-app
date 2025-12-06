22b969526ae188815b2d8b9dea128d7ae0e79f85
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-06 10:23:41 +0300

    Добавил ДТО для обновления коммента

--- src/main/java/com/alex/blog/service/impl/CommentServiceImpl.java
+++ src/main/java/com/alex/blog/service/impl/CommentServiceImpl.java
@@ -9,10 +9,7 @@ import com.alex.blog.mapper.CommentMapper;
 import com.alex.blog.model.Comment;
 import com.alex.blog.repository.CommentRepository;
 import com.alex.blog.repository.PostManagementRepository;
-import com.alex.blog.repository.PostSearchRepository;
 import com.alex.blog.service.CommentService;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -27,7 +24,70 @@ public class CommentServiceImpl implements CommentService {
     private final PostManagementRepository postManagementRepository;
 
 
+    @Override
+    public CommentReadDto findOneComment(Long postId, Long commentId) {
 
+        return commentRepository.findById(commentId)
+                .map(commentMapper::toCommentReadDto)
+                .orElseThrow(() -> new EntityNotFoundException("The comment with id: %d not found"
+                        .formatted(commentId)));
+    }
+
+    @Override
+    public CommentReadDto saveComment(Long postId, CommentCreateDto commentCreateDto) {
+
+        if (!postManagementRepository.existsById(postId)) {
+            throw new EntityNotFoundException("The post with id: %d not found"
+                    .formatted(commentCreateDto.postId()));
+        }
+       CommentReadDto commentReadDto=Optional.ofNullable(commentCreateDto)
+                .map(commentMapper::toComment)
+                .map(commentRepository::save)
+                .map(commentMapper::toCommentReadDto)
+               .orElseThrow(()-> new EntityCreationException("An error has been detected during creating"));
+
+        postManagementRepository.incrementCommentsCount(commentReadDto.postId(),1L);
+
+        return commentReadDto;
+    }
+
+    @Override
+    public CommentReadDto updateComment(Long postId, Long commentId, CommentUpdateDto commentUpdateDto) {
+        if (!postManagementRepository.existsById(postId)) {
+            throw new EntityNotFoundException("The post with id: %d not exists".formatted(postId));
+        }
+        Optional<Comment> maybeComment = Optional.ofNullable(commentId)
+                .flatMap(commentRepository::findById)
+                .map(comm -> {
+                    commentMapper.updateComment(commentUpdateDto, comm);
+                    return comm;
+                });
+
+
+        if (maybeComment.isEmpty()) {
+            throw new EntityNotFoundException("The comment with id: %d not found".formatted(commentId));
+        }
+
+        Comment updatedComment = commentRepository.update(maybeComment.get());
+
+        return commentMapper.toCommentReadDto(updatedComment);
+
+    }
+
+    @Override
+    public void deleteComment(Long postId, Long commentId) {
+        if (!postManagementRepository.existsById(postId)) {
+            throw new EntityNotFoundException("The post with id: %d not exists".formatted(postId));
+        }
+        Optional<Comment> maybeComment = commentRepository.findById(commentId);
+
+        if (maybeComment.isEmpty()) {
+            throw new EntityNotFoundException("The comment with id: %d not found".formatted(commentId));
+        }
+        commentRepository.delete(commentId);
+        System.out.println(postManagementRepository.incrementCommentsCount(postId,-1L));
+
+    }
 
     @Override
     public List<CommentReadDto> findCommentsByPostId(Long postId) {
