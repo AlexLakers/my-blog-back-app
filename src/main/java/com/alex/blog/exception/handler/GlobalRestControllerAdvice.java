67ef401138dcf2db0b171e96647f2fb067c8cac4
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-07 19:58:22 +0300

    Добавил исклбчения для обработки сохранения картинки в обработчик исключений.

--- src/main/java/com/alex/blog/exception/handler/GlobalRestControllerAdvice.java
+++ src/main/java/com/alex/blog/exception/handler/GlobalRestControllerAdvice.java
@@ -1,8 +1,6 @@
 package com.alex.blog.exception.handler;
 
-import com.alex.blog.exception.EntityNotFoundException;
-import com.alex.blog.exception.EntityCreationException;
-import com.alex.blog.exception.TitleAlreadyExistsException;
+import com.alex.blog.exception.*;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.springframework.http.ResponseEntity.status;
 
-@RestControllerAdvice(basePackages = "com.alex.blog.api.rest.controller.1")
+@RestControllerAdvice(basePackages = "com.alex.blog.api.rest.controller")
 public class GlobalRestControllerAdvice {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> handleCommentNotFoundException(EntityNotFoundException ex) {
@@ -21,8 +19,8 @@ public class GlobalRestControllerAdvice {
                 .body(ex.getMessage());
     }
 
-       @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleConstraintViolationException(MethodArgumentNotValidException ex) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleArgumentNotValidException(MethodArgumentNotValidException ex) {
         return status(HttpStatus.BAD_REQUEST)
                 .body(ex.getMessage());
     }
@@ -33,15 +31,29 @@ public class GlobalRestControllerAdvice {
         return status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ex.getMessage());
     }
+
     @ExceptionHandler(TitleAlreadyExistsException.class)
     public ResponseEntity<String> handleTitleAlreadyExistsException(TitleAlreadyExistsException ex) {
         return status(HttpStatus.BAD_REQUEST)
                 .body(ex.getMessage());
     }
 
+    @ExceptionHandler(ImageNotFoundException.class)
+    public ResponseEntity<String> handleImageNotFoundException(ImageNotFoundException ex) {
+        return status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ImageSavingException.class)
+    public ResponseEntity<String> handleImageSavingException(Exception ex) {
+        return status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ex.getMessage());
+    }
+
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception ex) {
         return status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body( ex.getMessage());
+                .body(ex.getMessage());
     }
 }
