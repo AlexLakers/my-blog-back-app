c7ee0ce5ef9d614e11897f4a59a7b3bb591d0843
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-10 21:26:57 +0300

    Добавил 1.конфиг для AopConfig 2. Два аспекта в LoggingAspect (общий по аннотации Loggable) и аспект для контроллеров чтобы просто сигнализировать факт захода в ендпоинт.

---
+++ src/main/java/com/alex/blog/aop/AopConfig.java
@@ -0,0 +1,9 @@
+package com.alex.blog.aop;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy
+@Configuration
+public class AopConfig {
+}
