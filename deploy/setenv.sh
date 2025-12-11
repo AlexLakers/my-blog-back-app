ef19d7072c1336dbe0623b98e8ae34adfb2c0e9b
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-11 18:13:16 +0300

    Добавил setenv.sh для устновки переменных среды в контейнере сервлетов.

---
+++ deploy/setenv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Variables for TOMCAT..."
+
+export DB_NAME="blog_repository"
+export DB_PORT="5432"
+export DB_USER="bloguser"
+export DB_HOST="localhost"
+export BLOG_STORAGE=/home/alexlakers/blog_storage
+export DB_PASSWORD=""
