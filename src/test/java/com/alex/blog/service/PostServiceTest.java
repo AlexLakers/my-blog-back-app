49e3dad73873518c66a1aacf1a259caa414d266b
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-15 23:11:19 +0300

    Добавил тесты для сервисов с использованием Spring Boot Test: PostServiceTest, CommentServiceTest

--- src/test/java/com/alex/blog/service/PostServiceTest.java
+++ src/test/java/com/alex/blog/service/PostServiceTest.java
@@ -18,9 +18,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -29,31 +31,26 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
 
-import static org.mockito.Mockito.reset;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.*;
 
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = TestServiceConfig.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class PostServiceTest {
 
-    @Autowired
+    @MockitoBean
     private PostManagementRepository postManagementRepository;
-    @Autowired
+    @MockitoBean
     private PostSearchRepository postSearchRepository;
-    @Autowired
+    @MockitoBean
     private PostMapper postMapper;
-    @Autowired
+    @MockitoBean
     CommentRepository commentRepository;
     @Autowired
     private MessageSource messageSource;
     @Autowired
     private PostService postService;
-
-    @BeforeEach
-    void resetMocks() {
-        reset(postManagementRepository,commentRepository,postSearchRepository,postMapper);
-    }
-
 
     private final static Long VALID_ID = 1L;
     private final static Long INVALID_ID = 10000L;
@@ -62,19 +59,19 @@ class PostServiceTest {
 
     @Test
     void findOnePost_shouldReturnPostSuccess() {
-        Mockito.when(postSearchRepository.findPostById(VALID_ID)).thenReturn(Optional.of(post));
-        Mockito.when(postMapper.toPostReadDto(post)).thenReturn(expectedDto);
+        when(postSearchRepository.findPostById(VALID_ID)).thenReturn(Optional.of(post));
+        when(postMapper.toPostReadDto(post)).thenReturn(expectedDto);
 
         PostReadDto actualDto = postService.findOnePost(VALID_ID);
 
-        Assertions.assertThat(actualDto).isEqualTo(expectedDto);
+        assertThat(actualDto).isEqualTo(expectedDto);
     }
 
     @Test
     void findOnePost_shouldPostThrowEntityNotFoundException() {
-        Mockito.when(postSearchRepository.findPostById(INVALID_ID)).thenReturn(Optional.empty());
+        when(postSearchRepository.findPostById(INVALID_ID)).thenReturn(Optional.empty());
 
-        Assertions.assertThatExceptionOfType(EntityNotFoundException.class)
+        assertThatExceptionOfType(EntityNotFoundException.class)
                 .isThrownBy(() -> postService.findOnePost(INVALID_ID))
                 .withMessage(messageSource.getMessage(MessageKey.POST_NOT_FOUND, new Object[]{INVALID_ID}, Locale.ENGLISH));
     }
@@ -83,19 +80,19 @@ class PostServiceTest {
     @Test
     void incrementLikesCount_shouldReturnIncLikesCountSuccess() {
         Long expectedLikes = 100L;
-        Mockito.when(postManagementRepository.existsById(VALID_ID)).thenReturn(true);
-        Mockito.when(postManagementRepository.incrementLikesCount(VALID_ID)).thenReturn(expectedLikes);
+        when(postManagementRepository.existsById(VALID_ID)).thenReturn(true);
+        when(postManagementRepository.incrementLikesCount(VALID_ID)).thenReturn(expectedLikes);
 
         Long actualLikes = postService.incrementLikesCount(VALID_ID);
 
-        Assertions.assertThat(actualLikes).isEqualTo(expectedLikes);
+        assertThat(actualLikes).isEqualTo(expectedLikes);
     }
 
     @Test
     void incrementLikesCount_shouldPostThrowEntityNotFoundException() {
-        Mockito.when(postManagementRepository.existsById(INVALID_ID)).thenReturn(false);
+        when(postManagementRepository.existsById(INVALID_ID)).thenReturn(false);
 
-        Assertions.assertThatExceptionOfType(EntityNotFoundException.class)
+        assertThatExceptionOfType(EntityNotFoundException.class)
                 .isThrownBy(() -> postService.incrementLikesCount(INVALID_ID))
                 .withMessage(messageSource.getMessage(MessageKey.POST_NOT_FOUND, new Object[]{INVALID_ID}, Locale.ENGLISH));
     }
@@ -107,22 +104,22 @@ class PostServiceTest {
     @Test
     void updatePost_shouldReturnUpdatedPostSuccess() {
         PostUpdateDto givenDto = new PostUpdateDto(VALID_ID, "testTitle", "testText", List.of("test_tag1"));
-        Mockito.when(postSearchRepository.findPostById(VALID_ID)).thenReturn(Optional.of(post));
-        Mockito.doNothing().when(postMapper).updatePost(givenDto, post);
-        Mockito.when(postManagementRepository.update(Mockito.any(Post.class))).thenReturn(post);
-        Mockito.when(postMapper.toPostReadDto(post)).thenReturn(expectedDto);
+        when(postSearchRepository.findPostById(VALID_ID)).thenReturn(Optional.of(post));
+        doNothing().when(postMapper).updatePost(givenDto, post);
+        when(postManagementRepository.update(Mockito.any(Post.class))).thenReturn(post);
+        when(postMapper.toPostReadDto(post)).thenReturn(expectedDto);
 
         PostReadDto actualDto = postService.updatePost(VALID_ID, givenDto);
 
-        Assertions.assertThat(actualDto).isEqualTo(expectedDto);
+        assertThat(actualDto).isEqualTo(expectedDto);
     }
 
     @Test
     void updatePost_shouldPostThrowEntityNotFoundExceptionFail() {
         PostUpdateDto givenDto = new PostUpdateDto(INVALID_ID, "testTitle", "testText", List.of("test_tag1"));
-        Mockito.when(postSearchRepository.findPostById(INVALID_ID)).thenReturn(Optional.empty());
+        when(postSearchRepository.findPostById(INVALID_ID)).thenReturn(Optional.empty());
 
-        Assertions.assertThatExceptionOfType(EntityNotFoundException.class)
+        assertThatExceptionOfType(EntityNotFoundException.class)
                 .isThrownBy(() -> postService.updatePost(INVALID_ID, givenDto))
                 .withMessage(messageSource.getMessage(MessageKey.POST_NOT_FOUND, new Object[]{INVALID_ID}, Locale.ENGLISH));
 
@@ -133,57 +130,57 @@ class PostServiceTest {
     void savePost_shouldReturnPersistentPostSuccess() {
         PostReadDto expectedDto = new PostReadDto(1L, "testTitle1", "test desc1", List.of("test_tag1"), 2L, 3L);
         PostCreateDto givenDto = new PostCreateDto("testTitle", "test text", List.of("test_tag1"));
-        Mockito.when(postManagementRepository.existsByTitle(givenDto.title())).thenReturn(false);
-        Mockito.when(postMapper.toPost(Mockito.any(PostCreateDto.class))).thenReturn(post);
-        Mockito.when(postManagementRepository.save(Mockito.any(Post.class))).thenReturn(post);
-        Mockito.when(postMapper.toPostReadDto(post)).thenReturn(expectedDto);
+        when(postManagementRepository.existsByTitle(givenDto.title())).thenReturn(false);
+        when(postMapper.toPost(Mockito.any(PostCreateDto.class))).thenReturn(post);
+        when(postManagementRepository.save(Mockito.any(Post.class))).thenReturn(post);
+        when(postMapper.toPostReadDto(post)).thenReturn(expectedDto);
 
         PostReadDto actualDto = postService.savePost(givenDto);
 
-        Assertions.assertThat(actualDto).isEqualTo(expectedDto);
+        assertThat(actualDto).isEqualTo(expectedDto);
 
-        Mockito.verify(postManagementRepository, Mockito.times(1)).save(post);
-        Mockito.verify(postMapper, Mockito.times(1)).toPostReadDto(post);
+        verify(postManagementRepository, Mockito.times(1)).save(post);
+        verify(postMapper, Mockito.times(1)).toPostReadDto(post);
 
     }
 
     @Test
     void savePost_shouldThrowTitleAlreadyExistsExceptionFail() {
         PostCreateDto givenDto = new PostCreateDto("testTitle", "test text", List.of("test_tag1"));
-        Mockito.when(postManagementRepository.existsByTitle(givenDto.title())).thenReturn(true);
+        when(postManagementRepository.existsByTitle(givenDto.title())).thenReturn(true);
 
-        Assertions.assertThatExceptionOfType(TitleAlreadyExistsException.class)
+        assertThatExceptionOfType(TitleAlreadyExistsException.class)
                 .isThrownBy(() -> postService.savePost(givenDto))
                 .withMessage(messageSource.getMessage(MessageKey.POST_TITLE_EXISTS_EX, new Object[]{givenDto.title()}, Locale.ENGLISH));
 
-        Mockito.verify(postManagementRepository, Mockito.times(0)).save(post);
-        Mockito.verify(postMapper, Mockito.times(0)).toPostReadDto(post);
+        verify(postManagementRepository, Mockito.times(0)).save(post);
+        verify(postMapper, Mockito.times(0)).toPostReadDto(post);
     }
 
     @Test
     void deletePost_shouldCallDeletePostSuccess() {
-        Mockito.when(postManagementRepository.existsById(VALID_ID)).thenReturn(true);
-        Mockito.doNothing().when(postManagementRepository).delete(VALID_ID);
-        Mockito.doNothing().when(commentRepository).deleteByPostId(VALID_ID);
+        when(postManagementRepository.existsById(VALID_ID)).thenReturn(true);
+        doNothing().when(postManagementRepository).delete(VALID_ID);
+        doNothing().when(commentRepository).deleteByPostId(VALID_ID);
 
         postService.deletePost(VALID_ID);
 
-        Mockito.verify(postManagementRepository, Mockito.times(1)).delete(VALID_ID);
-        Mockito.verify(commentRepository, Mockito.times(1)).deleteByPostId(VALID_ID);
+        verify(postManagementRepository, Mockito.times(1)).delete(VALID_ID);
+        verify(commentRepository, Mockito.times(1)).deleteByPostId(VALID_ID);
 
     }
 
     @Test
     void deletePost_shouldThrowEntityNotFoundException() {
-        Mockito.when(postManagementRepository.existsById(INVALID_ID)).thenReturn(false);
+        when(postManagementRepository.existsById(INVALID_ID)).thenReturn(false);
 
 
-        Assertions.assertThatExceptionOfType(EntityNotFoundException.class)
+        assertThatExceptionOfType(EntityNotFoundException.class)
                 .isThrownBy(() -> postService.deletePost(INVALID_ID))
                 .withMessage(messageSource.getMessage(MessageKey.POST_NOT_FOUND, new Object[]{INVALID_ID}, Locale.ENGLISH));
 
-        Mockito.verify(postManagementRepository, Mockito.times(0)).delete(INVALID_ID);
-        Mockito.verify(commentRepository, Mockito.times(0)).deleteByPostId(INVALID_ID);
+        verify(postManagementRepository, Mockito.times(0)).delete(INVALID_ID);
+        verify(commentRepository, Mockito.times(0)).deleteByPostId(INVALID_ID);
 
 
     }
@@ -191,33 +188,33 @@ class PostServiceTest {
     @Test
     void getImage_shouldReturnArrayBytesSuccess() {
         byte[] expectedImage = new byte[]{1, 2, 3, 4};
-        Mockito.when(postManagementRepository.existsById(VALID_ID)).thenReturn(true);
-        Mockito.when(postManagementRepository.getImage(VALID_ID)).thenReturn(Optional.of(expectedImage));
+        when(postManagementRepository.existsById(VALID_ID)).thenReturn(true);
+        when(postManagementRepository.getImage(VALID_ID)).thenReturn(Optional.of(expectedImage));
 
 
         byte[] image = postService.getImage(VALID_ID);
 
-        Assertions.assertThat(image).isEqualTo(expectedImage);
+        assertThat(image).isEqualTo(expectedImage);
     }
     @Test
     void getImage_shouldThrowEntityNotFoundExceptionFail() {
-        Mockito.when(postManagementRepository.existsById(INVALID_ID)).thenReturn(false);
+        when(postManagementRepository.existsById(INVALID_ID)).thenReturn(false);
 
-        Assertions.assertThatExceptionOfType(EntityNotFoundException.class)
+        assertThatExceptionOfType(EntityNotFoundException.class)
                 .isThrownBy(() -> postService.getImage(INVALID_ID))
                 .withMessage(messageSource.getMessage(MessageKey.POST_NOT_FOUND, new Object[]{INVALID_ID}, Locale.ENGLISH));
 
-        Mockito.verify(postManagementRepository, Mockito.times(0)).getImage(INVALID_ID);
+        verify(postManagementRepository, Mockito.times(0)).getImage(INVALID_ID);
     }
 
     @Test
     void updateImage_shouldCallSaveFileAndUpdateImageSuccess() throws IOException {
         MultipartFile image =new MockMultipartFile("image",new byte[]{1, 2, 3, 4});
-        Mockito.when(postManagementRepository.existsById(VALID_ID)).thenReturn(true);
-        Mockito.when(postManagementRepository.updateImage(VALID_ID,image.getBytes())).thenReturn(true);
+        when(postManagementRepository.existsById(VALID_ID)).thenReturn(true);
+        when(postManagementRepository.updateImage(VALID_ID,image.getBytes())).thenReturn(true);
 
         postService.updateImage(VALID_ID,image);
 
-        Mockito.verify(postManagementRepository,Mockito.times(1)).updateImage(VALID_ID,image.getBytes());
+        verify(postManagementRepository,Mockito.times(1)).updateImage(VALID_ID,image.getBytes());
     }
 }
