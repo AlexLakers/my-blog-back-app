7f29c30e7dc1915441ebbdedf4be403b225c8045
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-15 17:48:33 +0300

    Добавил application.yaml в тесты и в исходники. Добавил основные классы для запуска приложения в исходники и тесты. Изменил ГитИгнор файл

--- src/test/java/com/alex/blog/config/TestWebConfig.java
+++ src/test/java/com/alex/blog/config/TestWebConfig.java
@@ -1,22 +1,10 @@
 package com.alex.blog.config;
 
-import com.alex.blog.api.rest.controller.CommentRestController;
-import com.alex.blog.api.rest.controller.PostRestController;
-import com.alex.blog.config.RestConfiguration;
-import com.alex.blog.service.CommentService;
-import com.alex.blog.service.PostService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.mockito.Mockito;
 import org.springframework.context.annotation.*;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 @Import({TestMessagesConfig.class,TestMapperConfig.class})
